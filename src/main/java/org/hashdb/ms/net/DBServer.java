0a354cea0e200ad3a2ecb0530a0c5cae92c6d783
Wei_Zihang <3508077956@qq.com>
2023-12-07 12:37:34 +0800

    add some annotations

--- src/main/java/org/hashdb/ms/net/DBServer.java
+++ src/main/java/org/hashdb/ms/net/DBServer.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.hashdb.ms.compiler.CommandExecutor;
 import org.hashdb.ms.config.DBServerConfig;
 import org.hashdb.ms.config.ReplicationConfig;
+import org.hashdb.ms.constant.ServerIdentity;
 import org.hashdb.ms.event.StartServerEvent;
 import org.hashdb.ms.exception.*;
 import org.hashdb.ms.net.client.CommandMessage;
 import org.hashdb.ms.net.msg.Message;
 import org.hashdb.ms.net.service.ActCommandMessage;
 import org.hashdb.ms.net.service.ErrorMessage;
+import org.hashdb.ms.sys.DBSystem;
 import org.hashdb.ms.util.AsyncService;
 import org.hashdb.ms.util.Runners;
 import org.jetbrains.annotations.Contract;
@@ -40,20 +42,18 @@ public class DBServer implements DisposableBean {
 
     private final DBServerConfig serverConfig;
 
-    private final ReplicationConfig replicationConfig;
+    private final DBSystem dbSystem;
 
-    public DBServer(DBServerConfig serverConfig, ReplicationConfig replicationConfig) {
+    public DBServer(DBServerConfig serverConfig, DBSystem system) {
         this.serverConfig = serverConfig;
-        this.replicationConfig = replicationConfig;
+        this.dbSystem = system;
     }
 
     @EventListener(StartServerEvent.class)
     public void startServer() {
         try {//开启服务器后先广播一次确认主机，然后再进行全量数据同步
-            broadcast();
-            broadcast(DBServerConfig serverConfig, DecideMasterMessage msg){
+            beginReplication(msg = new verificateMasterMessage());
 
-            };
             try {
                 serverChannel = ServerSocketChannel.open();
                 serverChannel.configureBlocking(true);
@@ -82,6 +82,15 @@ public class DBServer implements DisposableBean {
             handleNewSession(connection);
         }
     }
+
+    private void beginReplication(verificateMasterMessage msg){
+        ReplicationConfig config = dbSystem.getReplicationConfig();
+        if(config.getIdentity() == ServerIdentity.MASTER){
+            //接受其他从机发来的连接
+        }else {
+            //从机向主机发起连接
+        }
+    };
 
     private void handleNewSession(SocketChannel con) {
         AsyncService.submit(() -> {
